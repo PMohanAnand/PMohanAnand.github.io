2bfd25e20492718f5fe67c9c3559536edf2fef07
PMohanAnand <8686938+PMohanAnand@users.noreply.github.com>
2025-02-11 12:00:23 +0000

    Deploying to gh-pages from @ PMohanAnand/PMohanAnand.github.io@3fc6030e345efe15e47dae0cf57f0dc38094dc09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-randart-is-accepted-at-encycris-2025-workshop-at-the-icse-conference",
+          title: 'Our paper RANDART is accepted at EnCyCriS 2025 workshop at the ICSE conference...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
